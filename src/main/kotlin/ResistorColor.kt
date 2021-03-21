dafeada2fe2d8c9cc5313a66f263368fab01f3d1
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-21 10:16:42 +0400

    Add completed solution

--- src/main/kotlin/ResistorColor.kt
+++ src/main/kotlin/ResistorColor.kt
@@ -1,11 +1,10 @@
 object ResistorColor {
+    private val colorMap : Map<String, Int> = mapOf(
+        "black" to 0, "brown" to 1, "red" to 2, "orange" to 3, "yellow" to 4,
+        "green" to 5, "blue" to 6, "violet" to 7, "grey" to 8, "white" to 9
+    )
 
-    fun colorCode(input: String): Int {
-        TODO("Implement this to complete the task")
-    }
+    fun colorCode(input: String): Int? = colorMap[input.toLowerCase()]
 
-    fun colors(): List<String> {
-        TODO("Implement this to complete the task")
-    }
-
+    fun colors(): List<String> = colorMap.keys.toList()
 }
